512530fc318bd17ec0adecb1aaa4714aa6f51b40
Otlowski <otlowski.maciej@gmail.com>
2016-11-25 01:06:24 +0100

    [add] new migrations cars_parts and cars {update} junkyards

---
+++ database/migrations/2016_11_24_210207_update_junkyards_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateJunkyardsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+         Schema::table('junkyards', function($table) {
+                $table->integer('phone_number')->after('location')->nullable();
+            });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+         Schema::table('junkyards', function($table) {
+                $table->dropColumn('phone_number');
+            });
+    }
+}
---
+++ database/migrations/2016_11_24_233455_create_cars_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCarsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cars', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('users__id');
+            $table->integer('junkyards__id');
+            $table->string('car_brand');
+            $table->string('car_model');
+            $table->string('price');
+            $table->string('production_year');
+            $table->string('mileage');
+            $table->enum('fuel',['petrol','diesel','gas']);
+            $table->integer('power');
+            $table->string('capacity');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('cars');
+    }
+}
